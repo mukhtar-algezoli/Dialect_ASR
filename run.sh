0ca71c5bd2d9308fecbebb6dd72131b76819867a
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-10-28 18:25:01 +0300

    amend bash file

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@
 cd ~/projects/def-mageed/mukh/Dialect_ASR
 module purge
 module load python/3.7.9 scipy-stack
+pip install -r requirements.txt
 source ~/py37/bin/activate
 
-python main.py
+python main.py --device cpu
