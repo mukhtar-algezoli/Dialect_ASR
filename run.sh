661020b81f3d08f45f9aeaa0fc40f668de33f353
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-10-28 18:23:34 +0300

    amend bash file

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=<youremail@gmail.com>
 #SBATCH --mail-type=ALL
 
-cd ~/$projects/your-project
+cd ~/projects/def-mageed/mukh/Dialect_ASR
 module purge
 module load python/3.7.9 scipy-stack
 source ~/py37/bin/activate
