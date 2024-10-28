5562e47efdf5e3ffcfade96426361c75e0d9ea4c
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-10-28 18:21:55 +0300

    amend bash file

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=15:0:0    
+#SBATCH --mail-user=<youremail@gmail.com>
+#SBATCH --mail-type=ALL
 
-#SBATCH --job-name=0.1-S0     # Job name
-#SBATCH --output=/l/users/mukhtar.mohamed/output.txt
-#SBATCH --nodes=1                   # Run all processes on a single node   
-#SBATCH --ntasks=1              # Run on a single CPU
-#SBATCH --cpus-per-task=64           # Number of CPU cores (changed from 64 to 4)
-#SBATCH --mem=40G   
-#SBATCH --gres=gpu:1                # Number of GPUs (per node)
-#SBATCH -p gpu                      # Use the gpu partition
-#SBATCH --time=8:00:00             # Specify the time needed for your experiment
-#SBATCH --qos gpu-8                 # To enable the use of up to 8 GPUs
+cd ~/$projects/your-project
+module purge
+module load python/3.7.9 scipy-stack
+source ~/py37/bin/activate
 
-hostname
- 
-# python /l/users/rzan.alhaddad/DUB-dysarthria/src/run_translate_with_pseudo.sh --additional_data 0.1 --severity 0
-python main.py --batch_size 16 --epochs 100 --EXP_name Binary_original --wandb_run_name Binary_original
+python main.py
