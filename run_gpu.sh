c22567f2ed7108ebd7fa095dfdd4945058800cc6
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-10-29 10:47:25 +0300

    adding gpu bash script

---
+++ run_gpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1 
+#SBATCH --gpus-per-node=a100:1   
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8    # There are 24 CPU cores on P100 Cedar GPU nodes
+#SBATCH --mem=0               # Request the full memory of the node
+
+cd ~/projects/def-mageed/mukh/Dialect_ASR
+module purge
+module load python/3.7.9 scipy-stack
+pip install -r requirements.txt
+source ~/py37/bin/activate
+
+python main.py 
