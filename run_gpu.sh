ef88074ba3b0c7e84acd2030255dc2b188da0e7a
mukhtar-algezoli <mukhtaralgezoli@gmail.com>
2024-10-29 11:00:49 +0300

    adding gpu bash script

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:4
+#SBATCH --gpus-per-node=p100:1
 #SBATCH --ntasks-per-node=24
-#SBATCH --exclusive
 #SBATCH --mem=125G
 #SBATCH --time=3:00
 #SBATCH --account=def-mageed
